b0c1e74ebd390013c9e8042606c9255158ad32d3
Akash. S <115996111+AkAsH-2pac@users.noreply.github.com>
2023-09-04 15:16:28 +0530

    Update Legal_text_processing.py

--- Legal_text_processing.py
+++ Legal_text_processing.py
@@ -90,3 +90,51 @@ while True:
 
     # Add the conversation to buffer memory
     add_to_buffer_memory(uer_input, response)
+
+# Import necessary libraries and download NLTK data
+
+# Sample dataset (replace this with your own data)
+data = [
+    # Your existing questions and responses here
+]
+
+# Slang responses
+slang_responses = {
+    "What is chemistry?": "Yo, chemistry is all about mixin' stuff and seein' what happens.",
+    "Tell me about chemical elements.": "Chemical elements are like the building blocks of the universe, bro.",
+    "How does a chemical reaction work?": "Chemical reactions? It's like magic, man! Stuff changes into other stuff.",
+    "Explain the periodic table.": "The periodic table is like a cheat sheet for all the elements. It's how we keep track of 'em.",
+    "What are some common compounds?": "Common compounds? You mean like water and stuff? Yeah, they're everywhere."
+}
+
+# Function to get a slang response
+def get_slang_response(user_input):
+    response = slang_responses.get(user_input, "I ain't got a clue, bro.")
+    return response
+
+# Rest of your code remains the same
+
+# Chatbot loop
+print("Chatbot: Hey yo, shoot the questions, homie!")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        print("Chatbot: Peace out, amigo!")
+        break
+
+    # Check buffer memory for a relevant response
+    for user_msg, responses in buffer_memory.items():
+        if user_msg == user_input:
+            print("Chatbot: You asked me this before, here's what I said:")
+            for response in responses:
+                print(f"Chatbot: {response}")
+            break
+    else:
+        # If not found in buffer memory, generate a new response
+        response = get_response(user_input)
+        slang_response = random.choice(slang_responses) + response
+        print(slang_response)
+
+    # Add the conversation to buffer memory
+    add_to_buffer_memory(user_input, response)
+
